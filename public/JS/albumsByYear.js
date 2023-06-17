273bab42f2172a050dea30bb1b43515c8f431274
Zamza584 <mzamalloa97@gmail.com>
2023-06-17 01:50:08 -0500

    adding some features

--- public/JS/albumsByYear.js
+++ public/JS/albumsByYear.js
@@ -29,59 +29,65 @@ document.querySelector("#year").addEventListener("change", async (e) => {
     i++;
   }
 });
+let next = document.querySelectorAll(".next");
+for (var i = 0; i < next.length; i++) {
+  next[i].addEventListener("click", async (e) => {
+    let year = document.querySelector("#year").value;
+    let url = "https://album-by-year-api.onrender.com/" + `albums/${year}/next`;
 
-document.querySelector(".next").addEventListener("click", async (e) => {
-  let year = document.querySelector("#year").value;
-  let url = "https://album-by-year-api.onrender.com/" + `albums/${year}/next`;
+    let albums;
+    try {
+      let response = await fetch(url);
+      albums = await response.json();
+    } catch {
+      location.reload();
+    }
 
-  let albums;
-  try {
-    let response = await fetch(url);
-    albums = await response.json();
-  } catch {
-    location.reload();
-  }
+    let albumCards = document.querySelectorAll(".album-container__card");
 
-  let albumCards = document.querySelectorAll(".album-container__card");
-
-  let i = 0;
-  for (let card of albumCards) {
-    card.innerHTML = `<h2 name="albumName" value="${albums[i].name}">${albums[i].name}</h2>
+    let i = 0;
+    for (let card of albumCards) {
+      card.innerHTML = `<h2 name="albumName" value="${albums[i].name}">${albums[i].name}</h2>
     <p name="albumArtist" value="${albums[i].artist}"> ${albums[i].artist}</p>
     <p name="albumRelease" value="${albums[i].release_date}">${albums[i].release_date}</p>
     <a name="albumLink" value="${albums[i].spotify_link}" href="${albums[i].spotify_link}" target="_blank">Listen now on Spotify</a>
     <p name="albumTTracks" value=${albums[i].total_tracks}>Total tracks: ${albums[i].total_tracks}</p>
     <button class="btn favorites">Add to favorites</button>
     <img name="albumImage" src="${albums[i].image}" value="${albums[i].image}" alt="${albums[i].name} album cover">`;
-    i++;
-  }
-});
-document.querySelector(".back").addEventListener("click", async (e) => {
-  let year = document.querySelector("#year").value;
-  let url = "https://album-by-year-api.onrender.com/" + `albums/${year}/null/previous`;
+      i++;
+    }
+  });
+}
 
-  let albums;
-  try {
-    let response = await fetch(url);
-    albums = await response.json();
-  } catch {
-    location.reload();
-  }
+let back = document.querySelectorAll(".back");
+for (var i = 0; i < back.length; i++) {
+  document.querySelector(".back").addEventListener("click", async (e) => {
+    let year = document.querySelector("#year").value;
+    let url = "https://album-by-year-api.onrender.com/" + `albums/${year}/null/previous`;
 
-  let albumCards = document.querySelectorAll(".album-container__card");
+    let albums;
+    try {
+      let response = await fetch(url);
+      albums = await response.json();
+    } catch {
+      location.reload();
+    }
 
-  let i = 0;
-  for (let card of albumCards) {
-    card.innerHTML = `<h2 name="albumName" value="${albums[i].name}">${albums[i].name}</h2>
+    let albumCards = document.querySelectorAll(".album-container__card");
+
+    let i = 0;
+    for (let card of albumCards) {
+      card.innerHTML = `<h2 name="albumName" value="${albums[i].name}">${albums[i].name}</h2>
     <p name="albumArtist" value="${albums[i].artist}"> ${albums[i].artist}</p>
     <p name="albumRelease" value="${albums[i].release_date}">${albums[i].release_date}</p>
     <a name="albumLink" value="${albums[i].spotify_link}" href="${albums[i].spotify_link}" target="_blank">Listen now on Spotify</a>
     <p name="albumTTracks" value=${albums[i].total_tracks}>Total tracks: ${albums[i].total_tracks}</p>
     <button class="btn favorites">Add to favorites</button>
     <img name="albumImage" src="${albums[i].image}" value="${albums[i].image}" alt="${albums[i].name} album cover">`;
-    i++;
-  }
-});
+      i++;
+    }
+  });
+}
 
 let list = document.querySelectorAll(".favorites");
 for (var i = 0; i < list.length; i++) {
